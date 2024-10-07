a6c2eb806ab6faa3bbd9ce5ba29d3c997498e785
balaji2004 <cvbalaji19672004@gmail.com>
2024-10-07 11:44:50 +0530

    challenge1-trail1

--- src/App.cpp
+++ src/App.cpp
@@ -2,10 +2,16 @@
 #include <fstream>
 #include <sstream>
 #include <string>
-
+using namespace std;
 int main(int argc, char** argv)
 {
 	// Put your code here!
-
-	return 0;
+	string s;
+	cin>>s;
+	if (s=="")
+	{
+		return 0;
+	}
+	
+	else return 1;
 }
