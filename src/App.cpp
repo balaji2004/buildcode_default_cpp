32b5c78e7b562f605ec7520f4acc68d51c612c2e
balaji2004 <cvbalaji19672004@gmail.com>
2024-10-07 13:10:56 +0530

    Update App.cpp

--- src/App.cpp
+++ src/App.cpp
@@ -2,29 +2,29 @@
 #include <fstream>
 #include <sstream>
 #include <string>
-
+using namespace std;
 
 int main(int argc, char* argv[]) {
    
     if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <pattern> <filename>" << std::endl;
+        cerr << "Usage: " << argv[0] << " <pattern> <filename>" << endl;
         return 1;  // Error if arguments are incorrect
     }
 
     // Read the pattern (which will be empty in this case)
-    std::string pattern = argv[1];
+    string pattern = argv[1];
     
     // Open the file
-    std::ifstream file(argv[2]);
+    ifstream file(argv[2]);
     if (!file.is_open()) {
-        std::cerr << "Error: Could not open file " << argv[2] << std::endl;
+        cerr << "Error: Could not open file " << argv[2] <<endl;
         return 1;  // Error if file cannot be opened
     }
 
     // If the pattern is empty, we print the entire file
-    std::string line;
-    while (std::getline(file, line)) {
-        std::cout << line << std::endl;
+    string line;
+    while (getline(file, line)) {
+        cout << line << endl;
     }
 
     // Close the file
