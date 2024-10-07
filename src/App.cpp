c2ae9d2e8601c2ee2b02ca889e0574a9f7d1569e
balaji2004 <cvbalaji19672004@gmail.com>
2024-10-07 11:54:08 +0530

    Update App.cpp

--- src/App.cpp
+++ src/App.cpp
@@ -2,16 +2,34 @@
 #include <fstream>
 #include <sstream>
 #include <string>
-using namespace std;
-int main(int argc, char** argv)
-{
-	// Put your code here!
-	string s;
-	cin>>s;
-	if (s=="")
-	{
-		return 0;
-	}
-	
-	else return 1;
+
+
+int main(int argc, char* argv[]) {
+   
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <pattern> <filename>" << std::endl;
+        return 1;  // Error if arguments are incorrect
+    }
+
+    // Read the pattern (which will be empty in this case)
+    std::string pattern = argv[1];
+    
+    // Open the file
+    std::ifstream file(argv[2]);
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open file " << argv[2] << std::endl;
+        return 1;  // Error if file cannot be opened
+    }
+
+    // If the pattern is empty, we print the entire file
+    std::string line;
+    while (std::getline(file, line)) {
+        std::cout << line << std::endl;
+    }
+
+    // Close the file
+    file.close();
+
+    // Return 0 since an empty string always "matches"
+    return 0;
 }
